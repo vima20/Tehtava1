b4886d0894bc14025e4c0618402520ebad54d4fb
vima20 <122276969+vima20@users.noreply.github.com>
2024-05-29 11:28:06 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,51 +1,35 @@
-import React, { useState } from 'react';
-
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  );
-};
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {totalExercises}</p>;
-};
+import React from 'react';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const [parts, setParts] = useState([
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]);
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>{course.name}</h1>
+      {course.parts.map(part =>
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      )}
+      <p>Number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
     </div>
-  );
-};
+  )
+}
 
 export default App;
