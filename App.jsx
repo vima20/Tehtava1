ce3c8f60afeee19e556ee2aa36ef1224838aa703
vima20 <122276969+vima20@users.noreply.github.com>
2024-05-29 11:30:19 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -17,19 +17,24 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
+  };
 
   return (
     <div>
       <h1>{course.name}</h1>
-      {course.parts.map(part =>
+      {course.parts.map(part => (
         <p key={part.name}>
           {part.name} {part.exercises}
         </p>
-      )}
-      <p>Number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+      ))}
+      <p>
+        <strong>
+          Total number of exercises: {' '}
+          {course.parts.reduce((sum, part) => sum + part.exercises, 0)}
+        </strong>
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
