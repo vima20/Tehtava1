ce146ce667bc352a6aa87633070b31528b5d24b7
vima20 <122276969+vima20@users.noreply.github.com>
2024-05-29 11:53:03 +0300

    Add files via upload

---
+++ App1.6.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  // Tilamuuttujat
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // Palautteen käsittelyfunktiot
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
+
+export default App;
